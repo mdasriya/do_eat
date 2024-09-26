369ccb0be9827f82b6731939f742dd9072d6ee64
Mukesh Dasriya <110367868+mdasriya@users.noreply.github.com>
2024-09-27 00:32:50 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import StorecontextProvider from "./context/StoreContext.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("")).render(
   <BrowserRouter>
     <StorecontextProvider>
       <App />
